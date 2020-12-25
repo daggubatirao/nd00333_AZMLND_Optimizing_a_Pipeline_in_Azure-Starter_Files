6da1d24ad2655340f288f1dcda058900d896188a
daggubatirao <daggubati.rao@gmail.com>
2020-12-25 22:05:48 +0000

    Add files via upload

--- train_over.py
+++ train_over.py
@@ -3,7 +3,7 @@ import argparse
 import os
 import numpy as np
 from sklearn.metrics import mean_squared_error
-from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.metrics import roc_auc_score, roc_curve, f1_score
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
@@ -35,8 +35,23 @@ def clean_data(data):
     x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    return x_df, y_df
+    x_class0 = x_df[x_df['y']=='no']
+    x_class1 = x_df[x_df['y']=='yes']
+    class0_count = len(x_class0)
+    class1_count = len(x_class1)
+    if class1_count<class0_count:
+        x_class0_new = x_class0.sample(class1_count) #class_0.sample(class_count_1)
+        x_class1_new = x_class1
+    elif class1_count>class0_count:
+        x_class0_new = x_class0
+        x_class1_new = x_class1.sample(class0_count)
+    else:
+        x_class0_new = x_class0
+        x_class1_new = x_class1
+
+    x_new = pd.concat([x_class0_new, x_class1_new], axis=0)
+    y_df = x_new.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_new, y_df
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
@@ -72,6 +87,9 @@ def main():
     y_scores = model.predict_proba(x_test)
     auc = roc_auc_score(y_test,y_scores[:,1])    
     run.log('AUC', np.float(auc))
+
+    f1score = f1_score(y_test,y_scores,average='weighted')
+    run.log('F1_Score', np.float(f1score))
 
     os.makedirs('outputs', exist_ok=True)
     joblib.dump(value=model, filename='outputs/bankmarketing_model.pkl')
